c1e41620dd4bfde1fac90b4f17b6badc35c58295
SachinBishnoi-503 <bishnoisachin626@gmail.com>
2024-05-27 16:09:49 +0530

    section-2 completed  with responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,4 @@ let menuicon = document.querySelector(".menubar");
 let menulist = document.querySelector(".navbar-iteams");
 menuicon.addEventListener("click", function () {
     menulist.classList.toggle("show");
-})
+})  
